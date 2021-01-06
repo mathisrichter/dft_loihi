4eb35ab3059d527941f28705f0955e362a97e59d
Mathis Richter <mathis.richter@ini.rub.de>
2021-01-06 17:10:43 +0100

    Added 1D multi-peak example that shows the fusion of two inputs into a single peak.

---
+++ dft_loihi/examples/1d/1d_multi_peak_fusion.py
@@ -0,0 +1,72 @@
+import sys
+import numpy as np
+import nxsdk.net.net
+
+from dft_loihi.visualization.plotting import Plotter
+from dft_loihi.dft.field import Field
+from dft_loihi.dft.kernel import MultiPeakKernel
+from dft_loihi.inputs.simulated_input import PiecewiseStaticInput
+from dft_loihi.dft.util import connect
+
+sys.path.append("/home/mathis/uni/diss/code/python/dft_loihi")
+sys.path.append("/home/mathis/uni/diss/code/python/nxsdk/0.9.9/nxsdk-0.9.9")
+
+
+timesteps = 500
+net = nxsdk.net.net.NxNet()
+
+# setup the network
+field_domain = [-5, 5]
+field_shape = 30
+
+gauss_input1 = PiecewiseStaticInput("input 1", net, domain=field_domain, shape=field_shape)
+gauss_input1.add_gaussian_spike_rate(0, -0.775, 0.1, 100)
+gauss_input1.add_gaussian_spike_rate(11200, -0.775, 0.1, 400)
+gauss_input1.create()
+
+gauss_input2 = PiecewiseStaticInput("input 2", net, domain=field_domain, shape=field_shape)
+gauss_input2.add_gaussian_spike_rate(0, 0.775, 0.1, 100)
+gauss_input2.add_gaussian_spike_rate(11200, 0.775, 0.1, 400)
+gauss_input2.create()
+
+kernel = MultiPeakKernel(amp_exc=0.40,
+                         width_exc=2.0,
+                         amp_inh=-0.28,
+                         width_inh=4.0,
+                         border_type="zeros")
+
+field1 = Field("field",
+              net,
+              domain=field_domain,
+              shape=field_shape,
+              kernel=kernel,
+              tau_voltage=2,
+              tau_current=10,
+              delay=3)
+
+connect(gauss_input1, field1, 0.1, mask="one-to-one")
+connect(gauss_input2, field1, 0.1, mask="one-to-one")
+
+field2 = Field("field without kernel",
+              net,
+              domain=field_domain,
+              shape=field_shape,
+              kernel=None,
+              tau_voltage=2,
+              tau_current=10,
+              delay=3)
+
+connect(gauss_input1, field2, 0.1, mask="one-to-one")
+connect(gauss_input2, field2, 0.1, mask="one-to-one")
+
+# run the network
+net.run(timesteps)
+net.disconnect()
+
+# plot results
+plotter = Plotter()
+plotter.add_input_plot(gauss_input1)
+plotter.add_input_plot(gauss_input2)
+plotter.add_field_plot(field1)
+plotter.add_field_plot(field2)
+plotter.plot()
